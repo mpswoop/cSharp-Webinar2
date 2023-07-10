ca79b4656eb778a4684c2c7d3d8f655f05a586a6
MP <mp@swoop.pro>
2023-07-10 14:11:30 +0300

    task 4 done - square check

---
+++ tasks/task16/Program.cs
@@ -0,0 +1,14 @@
+﻿// square check - is one square of other
+Console.WriteLine("Input first number");
+int num1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Input second number");
+int num2 = Int32.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}

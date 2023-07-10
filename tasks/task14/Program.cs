5fec463fc03e22132f59bfdbbc9889e4049942cd
MP <mp@swoop.pro>
2023-07-10 14:04:40 +0300

    task 14 done- combined division check

---
+++ tasks/task14/Program.cs
@@ -0,0 +1,12 @@
+﻿// combined division check 7 & 23
+Console.WriteLine("Input first number");
+int num1 = Int32.Parse(Console.ReadLine());
+
+if (num1%7 == 0 & num1%23 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}

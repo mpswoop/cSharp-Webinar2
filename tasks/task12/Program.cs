74da06c51a49ed0a2ea8009351d584f34dad55ef
MP <mp@swoop.pro>
2023-07-10 13:57:38 +0300

    task 12 done - division check two numbers

---
+++ tasks/task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// enter two numbers, check if first can be devided by second, if yes show yes, if no show no
+// 16, 4 ==> yes
+// 18, 4 ==> 2
+Console.WriteLine("Input first number");
+int num1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Input second number");
+int num2 = Int32.Parse(Console.ReadLine());
+//int rest = num1 % num2;
+
+if (num1 % num2 == 0)
+// if (rest == 0)
+{
+    Console.WriteLine("");
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("");
+    Console.WriteLine("no, rest equals:");
+    Console.WriteLine(num1 % num2);  
+}

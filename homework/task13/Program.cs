4ba0deca87df7b03ec90877110e9d387eb1972f6
MP <mp@swoop.pro>
2023-07-11 10:25:25 +0300

    Task 13 done - third digt

---
+++ homework/task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// input number - output third digit from left or digit does not exist
+Console.WriteLine("please input a number");
+
+int num = Int32.Parse (Console.ReadLine());
+int length = num.ToString ().Length;
+
+if (length > 2)
+
+{
+int digit3 = num / 100 % 10;
+// int digit2 = num / 10 % 10;
+// int digit1 = num /= 100;
+
+// Console.WriteLine($"Digit1: {digit1}");
+// Console.WriteLine($"Digit2: {digit2}");
+Console.WriteLine($"Digit3: {digit3}");
+}
+else
+{
+    Console.WriteLine("there is no 3rd digit");
+}
